ba056044c23f58ec36259b44848590e4f7d91bb1
Changmo Kang <dkssudckdah@gmail.com>
2019-05-14 04:37:27 +0000

    충돌한다

--- movies/urls.py
+++ movies/urls.py
@@ -6,8 +6,9 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.movie_index, name='movie_index'),
-    path('<int:movie_id>/scores/', views.score_create_read, name='score_create_read'),
-    path('<int:movie_id>/scores/<int:score_id>/', views.score_update_delete, name='score_update_delete'),
+    path('<int:movie_id>/scores/create', views.score_create, name='score_create'),
+    path('<int:movie_id>/scores/<int:score_id>/update/', views.score_update, name='score_update'),
+    path('<int:movie_id>/scores/<int:score_id>/delete/', views.score_delete, name='score_delete'),
 ]
 
-urlpatterns += [path('docs/', get_swagger_view(title="API Document"))]
+# urlpatterns += [path('docs/', get_swagger_view(title="API Document"))]
--- movies/views.py
+++ movies/views.py
@@ -1,33 +1,72 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie, Score
-
-from django.contrib.auth import get_user_model
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import ScoreSerializer
 
 from .forms import ScoreModelForm
 
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+
 from .API_CALL.KOFIC.get_daily_list import daily_lists, YESTERDAY
-# from pprint import pprint
 
 
 # Create your views here.
 def movie_index(request):
-    return render(request, 'movies/index.html', {
+    return render(request, 'movies/practice.html', {
+        'movies': Movie.objects.all(),
         'date': YESTERDAY,
         'daily_lists': daily_lists,
-        'form': ScoreModelForm()
     })
-    
+
+
+@login_required
+@require_POST
+def score_create(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    score_form = ScoreModelForm(request.POST)
+    if score_form.is_valid():
+        score = score_form.save(commit=False)
+        score.movie = movie
+        score.user = request.user
+        score.save()
+    return redirect('movies:movie_index')
+
+
+@login_required
+@require_POST
+def score_update(request, movie_id, score_id):
+    score = get_object_or_404(Score, pk=score_id)
+    if request.method == 'POST':
+        score_form = ScoreModelForm(request.POST, instance=score)
+        if score_form.is_valid():
+            score_form.save()
+            return redirect('movies:movie_index')
+    else:
+        score_form = ScoreModelForm(instance=score)
+        return redirect("movies:movie_index") # render 해야 함 or Vue
+
+
+@login_required
+@require_POST
+def score_delete(request, movie_id, score_id):
+    score = get_object_or_404(Score, pk=score_id)
+    score.delete()
+    return redirect('movies:movie_index')
+
+
+
+""" REST API
+# from django.contrib.auth import get_user_model
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from .serializers import ScoreSerializer
+
 @api_view(['GET', 'POST'])
 def score_create_read(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     if request.method == 'POST':
         serializer = ScoreSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save(movie=movie, user=get_user_model())
+            serializer.save()
             msg_dict = { "message": "작성되었습니다." }
             return Response(msg_dict)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -52,3 +91,4 @@ def score_update_delete(request, movie_id, score_id):
         score.delete()
         msg_dict = { "message": "삭제되었습니다." }
         return Response(msg_dict)
+"""
