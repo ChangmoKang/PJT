f05a422887ba90cc84ed1cc8c399a37509fb0e38
Jiwon Yoon <white.jcme@gmail.com>
2019-05-15 05:16:55 +0000

    Add intro page

--- final/urls.py
+++ final/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from movies import views as movies_views
 
 urlpatterns = [
+    path('', movies_views.intro),
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
