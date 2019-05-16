c64bbeae8f62107c8b8332af0b0dccfef94e61e2
Changmo Kang <dkssudckdah@gmail.com>
2019-05-16 05:40:35 +0000

    정리

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Genre, Movie, Score
+from .models import Genre, Movie, Score, StillCut
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Movie)
 admin.site.register(Score)
+admin.site.register(StillCut)
--- movies/models.py
+++ movies/models.py
@@ -5,23 +5,34 @@ from django.conf import settings
 class Genre(models.Model):
     genreNm = models.CharField(default='', max_length=150)
     selected = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.genreNm
 
 
 class Nation(models.Model):
     nationNm = models.CharField(default='', max_length=150)
 
+    def __str__(self):
+        return self.nationNm
 
 class Director(models.Model):
     peopleNm = models.CharField(default='', max_length=150)
 
+    def __str__(self):
+        return self.peopleNm
 
 class Actor(models.Model):
     peopleNm = models.CharField(default='', max_length=150)
 
+    def __str__(self):
+        return self.peopleNm
 
 class StillCut(models.Model):
     stillCut = models.CharField(default='', max_length=150)
 
+    def __str__(self):
+        return self.stillCut
 
 class Movie(models.Model):
     movieCd = models.IntegerField(default=0)
@@ -49,6 +60,5 @@ class Movie(models.Model):
 class Score(models.Model):
     comment = models.TextField(default='')
     score = models.IntegerField(default=0)
-    movieCd = models.IntegerField(default=0)
-    # movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='scores')
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='scores')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='scores')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='scores')
