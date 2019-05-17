d00d4f3b60aa03f417688376a4b4aff13261e909
Jiwon Yoon <white.jcme@gmail.com>
2019-05-17 03:52:34 +0000

    heroku

--- final/settings.py
+++ final/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,7 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = "accounts.User"
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
